179a0a901ffa6af9a7ad02acc932063a23e2acaa
GameJing <learngame@126.com>
2023-02-14 17:58:33 +0800

    GetCurrentAnimatorStateInfo
    GetCurrentAnimatorStateInfo

--- code_unity_ai_2023/Assets/AppCode/GameCombatSystem2023/Player/System/RoleSystem/Role_AISystem.cs
+++ code_unity_ai_2023/Assets/AppCode/GameCombatSystem2023/Player/System/RoleSystem/Role_AISystem.cs
@@ -2,6 +2,7 @@
 角色AI系统，英雄和角色共用。英雄自动打，怪物行为
 
 */
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +20,8 @@ public class Role_AISystem : MonoBehaviour
     [Header("攻击目标")]
     public Transform attack_target;
 
+    private Animator animator;
+
     //[SerializeField]
     //public AnimationClip standAnimationClip;
     //[SerializeField]
@@ -28,13 +31,27 @@ public class Role_AISystem : MonoBehaviour
     //[SerializeField]
     //public AnimationClip attackAnimationClip;
 
+    private void Start()
+    {
+        SetComponent();
+        InitConfig();
+    }
 
+    private void SetComponent()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void InitConfig()
+    {
+        attack_trigger_distance = 5;
+    }
 
     /// <summary>
-    /// 动态距离-离主玩家的距离
+    /// 动态攻击距离
     /// </summary>
     /// <returns></returns>
-    public int CheckMainHeroDistance()
+    public int CheckAttackDistance()
     {
         return 4;
     }
@@ -42,6 +59,36 @@ public class Role_AISystem : MonoBehaviour
     public void LookAtTarget()
     {
         transform.LookAt(attack_target, Vector3.up);
-        //Debug.LogError("LookAtTarget OK!");
+        PlayAttack();
+        //Debug.LogError("面朝向目标 LookAtTarget OK!");
     }
+
+    #region Animator
+    private void PlayAttack()
+    {
+        if(animator == null)
+        {
+            SetComponent();
+            return;
+        }
+
+        if(GetAttackState() == false)
+        {
+            animator.SetTrigger("trigger_attack_1");
+        }
+    }
+
+    private bool GetAttackState()
+    {
+        var b = false;
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("attack_1"))
+        {
+            b = true;
+            //Debug.LogError("attack_1");
+        }
+
+        return b;
+    }
+
+    #endregion
 }
