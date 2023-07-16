66165952a854e96f78aa6ecbd93464513542b35f
Unity_Houdini <jingcrm@qq.com>
2023-07-17 07:54:39 +0800

    cube demo

---
+++ bitspicy_learn_unity_code/unity_demo_ecs_101/Assets/AppCode/ECS_Data/GameRoleMonster_Data.cs
@@ -0,0 +1,12 @@
+
+using Unity.Entities;
+using Unity.Mathematics;
+
+public struct GameRoleMonster_Data: IComponentData
+{
+    public float2 PosXY;
+    public int CountSpwan;
+    public Entity RolePrefab;
+
+
+}
